c45ce65ab4fc23ea8d9a5c61b8fb367cc8ebf0ac
RossyFranca <rossyfranca@gmail.com>
2024-02-05 05:32:53 -0300

    vaiiiii

--- src/test/java/runner/RunCucumberTest.java
+++ src/test/java/runner/RunCucumberTest.java
@@ -1,17 +1,19 @@
 package runner;
 
-import io.cucumber.junit.CucumberOptions;
+
+import io.cucumber.junit.Cucumber;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
+import org.junit.runner.RunWith;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "steps")
 @SelectClasspathResource("features")
+@RunWith(Cucumber.class)
 public class RunCucumberTest {
 }
