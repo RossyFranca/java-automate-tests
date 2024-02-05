9e30b069fa06ed8ebbeeb0ef0ef5e548c5b6e281
RossyFranca <rossyfranca@gmail.com>
2024-02-05 03:50:58 -0300

    ajuste

--- src/test/java/runner/RunCucumberTest.java
+++ src/test/java/runner/RunCucumberTest.java
@@ -1,5 +1,6 @@
 package runner;
 
+import io.cucumber.junit.CucumberOptions;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
@@ -10,6 +11,8 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
+@CucumberOptions(features = "src/test/resources/features",
+        glue = "src/test/java/stepDefinitions")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class RunCucumberTest {
 }
